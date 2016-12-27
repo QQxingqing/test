1b690fbf2c4a32dd8d1b17e7fc63ef3d03bb2126
QQxingqing <wangshipengdx@qq.com>
2016-12-27 18:21:07 +0800

    Delete 1.sh

--- exp/1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for i in `cat ./srv.txt`
-do
-                export server=`echo $i`
-                export cmdfile=`cat 1.txt`
-                ./1.exp $server $cmdfile
-		echo $server
-done
